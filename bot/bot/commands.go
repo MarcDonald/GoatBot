9c588a73294f330d7d512b892809577520102156
Marc Donald <marcsdeveloperemail@gmail.com>
2021-06-13 20:42:21 +0100

    Add ability to create custom commands

--- bot/bot/commands.go
+++ bot/bot/commands.go
@@ -1,10 +1,76 @@
 package bot
 
-type Command string
-
-const (
-	PING Command = "ping"
-	DISCORD Command = "discord"
-	TWITTER Command = "twitter"
-	INSTAGRAM Command = "instagram"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
 )
+
+type Command struct {
+	Invocation string  `json:"invocation"`
+	Message    string  `json:"message"`
+	ModOnly    bool    `json:"mod_only"`
+	Timer      float32 `json:"timer"`
+}
+
+var CommandList []Command
+
+func LoadCommands() {
+	log.Println("Loading commands")
+
+	var files []string
+
+	filepathError := filepath.Walk("commands/", func(path string, info os.FileInfo, err error) error {
+		files = append(files, path)
+		return nil
+	})
+
+	if filepathError != nil {
+		log.Fatalf(filepathError.Error())
+	}
+
+	for _, filePath := range files {
+		err := loadFile(filePath)
+		if err != nil {
+			log.Println("Error loading file " + filePath + ": " + err.Error())
+		}
+	}
+
+	log.Printf("%d commands successfully loaded\n", len(CommandList))
+}
+
+func loadFile(filePath string) error {
+	currentFile, err := os.Open(filePath)
+	if currentFile != nil {
+		defer func(currentFile *os.File) {
+			err := currentFile.Close()
+			if err != nil {
+				log.Println("Error closing " + filePath + ": " + err.Error())
+			}
+		}(currentFile)
+	}
+	if err != nil {
+		return err
+	}
+
+	fileInfo, err := currentFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	if !fileInfo.IsDir() {
+		fileData, fileReadErr := ioutil.ReadFile(filePath)
+
+		if fileReadErr != nil {
+			log.Println("Error loading fileData " + filePath)
+		}
+
+		commandFromFile := Command{}
+		_ = json.Unmarshal(fileData, &commandFromFile)
+
+		CommandList = append(CommandList, commandFromFile)
+	}
+	return nil
+}
--- bot/bot/twitchbot.go
+++ bot/bot/twitchbot.go
@@ -1,23 +1,23 @@
 package bot
 
 import (
-	"errors"
 	"github.com/gempir/go-twitch-irc/v2"
 	"log"
 	"os"
 	"strings"
 )
 
-var prefix, channel string
+var prefix, channel, nickname string
 
 func Init() {
 	prefix = os.Getenv("PREFIX")
+	channel = os.Getenv("CHANNEL")
+	nickname = os.Getenv("NAME")
+	LoadCommands()
 }
 
 func Start() {
-	nickname := os.Getenv("NAME")
-	channel = os.Getenv("CHANNEL")
-	oauth := os.Getenv("PASSWORD")
+	oauth := os.Getenv("SECRET")
 
 	client := twitch.NewClient(nickname, oauth)
 
@@ -43,32 +43,13 @@ func onMessage(client *twitch.Client, message twitch.PrivateMessage) {
 	}
 
 	if strings.HasPrefix(message.Message, prefix) {
-		err, command := parseCommand(message)
-		if err != nil {
-			if err.Error() == "no command detected" {
-				log.Println("Prefix used in message but no valid command detected: " + message.Message)
-			} else {
-				log.Println(err.Error())
+		commandString := getCommandStringFromMessage(message)
+		for _, command := range CommandList {
+			if command.Invocation == commandString {
+				if !command.ModOnly || (command.ModOnly && (message.User.Badges["moderator"] == 1) || (message.User.Badges["broadcaster"] == 1)) {
+					client.Say(channel, command.Message)
+				}
 			}
-		} else {
-			log.Println(">>>>>>>>>>>>>>>>>>>>>")
-			switch command {
-			case PING:
-				handlePing(client)
-				break
-			case DISCORD:
-				handleDiscord(client)
-				break
-			case TWITTER:
-				handleTwitter(client)
-				break
-			case INSTAGRAM:
-				handleInstagram(client)
-				break
-			default:
-				log.Println("Valid command detected but no implementation given")
-			}
-			log.Println("<<<<<<<<<<<<<<<<<<<<<")
 		}
 	}
 }
@@ -78,52 +59,7 @@ func parseMessageText(message twitch.PrivateMessage) string {
 	return strings.ToLower(messageText)
 }
 
-func parseCommand(message twitch.PrivateMessage) (error, Command) {
+func getCommandStringFromMessage(message twitch.PrivateMessage) string {
 	messageText := parseMessageText(message)
-	if strings.HasPrefix(messageText, string(PING)) {
-		return nil, PING
-	}
-	if strings.HasPrefix(messageText, string(DISCORD)) {
-		return nil, DISCORD
-	}
-	if strings.HasPrefix(messageText, string(TWITTER)) {
-		return nil, TWITTER
-	}
-	if strings.HasPrefix(messageText, string(INSTAGRAM)) {
-		return nil, INSTAGRAM
-	}
-	return errors.New("no command detected"), ""
-}
-
-func handlePing(client *twitch.Client) {
-	log.Println("Ping command received")
-	client.Say(channel, "Pong!")
-	log.Println("Ping command handled")
-}
-
-func handleDiscord(client *twitch.Client) {
-	log.Println("Discord command received")
-	discordLink := os.Getenv("DISCORD")
-	if discordLink != "" {
-		client.Say(channel, discordLink)
-	}
-	log.Println("Discord command handled")
-}
-
-func handleTwitter(client *twitch.Client) {
-	log.Println("Twitter command received")
-	twitterLink := os.Getenv("TWITTER")
-	if twitterLink != "" {
-		client.Say(channel, twitterLink)
-	}
-	log.Println("Twitter command handled")
-}
-
-func handleInstagram(client *twitch.Client) {
-	log.Println("Instagram command received")
-	instagramLink := os.Getenv("INSTAGRAM")
-	if instagramLink != "" {
-		client.Say(channel, instagramLink)
-	}
-	log.Println("Instagram command handled")
+	return strings.Split(messageText, " ")[0]
 }
